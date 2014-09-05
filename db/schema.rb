81ffb0130d740979ee41a59dc586cce8f7166021
vxvinod <vinodkmrmurugan@gmail.com>
2014-09-05 10:21:24 +0530

    column name changed

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,11 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140905034634) do
+ActiveRecord::Schema.define(:version => 20140905044652) do
 
   create_table "ushas", :force => true do |t|
     t.string   "title"
-    t.string   "type"
+    t.string   "food_type"
     t.string   "category"
     t.string   "video"
     t.text     "recipe"
